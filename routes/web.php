e25f2a6275deee192765c7ef92f20804757d3225
takeeshiii <panda3413@icloud.com>
2023-01-04 22:10:34 +0900

    ルート作成

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -23,3 +25,42 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// 商品一覧表示
+Auth::routes();
+
+Route::get('/product_view','TopController@productView')->name('product');
+
+// 検索
+Auth::routes();
+
+Route::get('/product','TopController@serch')->name('serch');
+
+
+// 削除
+Auth::routes();
+
+Route::post('/deleat{id}', 'TopController@deleat')->name('deleat');
+
+// 新規登録画面表示・登録
+Auth::routes();
+
+Route::get('/product_register','TopController@register')->name('register');
+
+Auth::routes();
+
+Route::post('/product_register','TopController@productRegister')->name('productRegister');
+
+// 詳細画面表示
+Auth::routes();
+
+Route::get('/product_detail','TopController@detail')->name('detail');
+
+// 編集画面表示・更新
+Auth::routes();
+
+Route::get('/product_update','TopController@update')->name('update');
+
+Auth::routes();
+
+Route::post('/product_update','TopController@productUpdate')->name('productUpdate');
